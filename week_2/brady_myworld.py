d28d6d0973e0e1b2b451b9923e4c64eb29c4fbd8
Just EnginErin <31420768+just-enginerin@users.noreply.github.com>
2023-03-27 23:40:30 -0400

    Exercise 2.3 - Python
    - Python environment setup
    - First Python program: Demonstrates how to print to the console.

---
+++ week_2/brady_myworld.py
@@ -0,0 +1,8 @@
+"""
+    Title: brady_myworld.py
+    Author: Erin Brady
+    Date: 22 March 2023
+    Description: First Python program: Demonstrates how to print to the console.
+"""
+
+print("You are now in Brady's world!")
